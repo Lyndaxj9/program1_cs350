22fb179079d19653cade7471cfab50837d372062
Nuri Ra <nra1@binghamton.edu>
2016-03-21 09:30:16 -0400

    finished up lab4

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -3,12 +3,25 @@
 #include <unistd.h>
 
 #include "prints.h"
+#include <time.h>
+
+void startProcess(int process, int pages) {
+    printf("START %d %d\n", process, pages);
+}
+
+void referencePage(int process, int page) {
+    printf("REFERENCE %d %d\n", process, page);
+}
+
+void terminateProcess(int process) {
+    printf("TERMINATE %d\n", process);
+}
 
 int main (int argc, char ** argv) {
-    if (argc == 1) {
-        printUsage();
-        return 0;
-    }
+    // if (argc == 1) {
+    //     printUsage();
+    //     return 0;
+    // }
     int u_opt, l_opt, p_opt, r_opt, R_opt, n_opt, N_opt;
     u_opt = l_opt = p_opt = r_opt = R_opt = n_opt = N_opt = 0;
 
@@ -27,37 +40,42 @@ int main (int argc, char ** argv) {
                 return 1;
             case 'l':
                 l_opt = 1;
+                break;
             case 'p':
                 p_opt = 1;
                 process_num = atoi(optarg);
-                printf("%d\n", process_num);
                 if (process_num < 1) {
                     printf("Invalid # of processes.\n");
                 }
+                break;
             case 'r':
                 r_opt = 1;
                 reference_min = atoi(optarg);
                 if (reference_min < 1) {
                     printf("Invalid min # of references.\n");
                 }
+                break;
             case 'R':
                 R_opt = 1;
                 reference_max = atoi(optarg);
                 if (reference_max == 0 || reference_max < reference_min) {
                     printf("Invalid max # of references.\n");
                 }
+                break;
             case 'n':
                 n_opt = 1;
                 page_min = atoi(optarg);
                 if (page_min < 1) {
                     printf("Invalid min # of pages per process.\n");
                 }
+                break;
             case 'N':
                 N_opt = 1;
                 page_max = atoi(optarg);
                 if (page_max < 1 || page_max < page_min) {
                     printf("Invalid max # of pages per process.\n");
                 }
+                break;
             case ':':
                 printNoArg(optopt);
                 return 1;
@@ -65,5 +83,69 @@ int main (int argc, char ** argv) {
                 printNoOpt(optopt);
                 return 1;
         }
+    }
+
+    // if (l_opt) {
+    //     printf("Locality enabled.\n");
+    // } else {
+    //     printf("Locality disabled.\n");
+    // }
+    // printf("# of processes: %d\n", process_num);
+    // printf("References: %d - %d\n", reference_min, reference_max);
+    // printf("Pages per process: %d - %d\n", page_min, page_max);
+
+    int processArray[process_num];
+    int referenceArray[process_num];
+    int pageNumArray[process_num];
+    int i;
+
+    for (i = 0; i < process_num; i++) {
+        processArray[i] = 0;
+        referenceArray[i] = rand() % (reference_max - reference_min) + reference_min;
+        pageNumArray[i] = rand() % (page_max - page_min) + page_min;
+    }
+
+    int procsRemaining = process_num;
+
+    int active = 10;
+    int current = 0;
+    int activeProcesses[active];
+    // printf("ACTIVE PROCESSES\n");
+    for (i = 0; i < active; i++) {
+        activeProcesses[i] = i;
+        processArray[i] = 1;
+        startProcess(i, pageNumArray[i]);
+        current++;
+    }
+
+    int localityCounter = 0;
+    int randomIndex;
+    while (procsRemaining > 0) {
+        if (l_opt) {
+            if (localityCounter % 10 == 0) {
+                randomIndex = rand() % active;
+            }
+        } else {
+            randomIndex = rand() % active;
+        }
+        int randomProcess = activeProcesses[randomIndex];
+
+        if (randomProcess != -1) {
+            referencePage(randomProcess, rand() % pageNumArray[randomProcess]);
+            referenceArray[randomProcess]--;
+            // printf("References: %d", referenceArray[randomProcess]);
+            if (referenceArray[randomProcess] == 0) {
+                terminateProcess(randomProcess);
+                processArray[randomProcess] = 2;
+
+                if (procsRemaining > 10) {
+                    activeProcesses[randomIndex] = current;
+                    startProcess(current, pageNumArray[current]);
+                    current++;
+                }
+                procsRemaining--;
+            }
+        }
+        localityCounter++;
     }
 }
