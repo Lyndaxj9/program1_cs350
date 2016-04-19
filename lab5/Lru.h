aaedd8cbbe290d80fd17be66d6c193e9a7370c2d
Daelin Fischman <dfischman1@gmail.com>
2016-04-19 10:08:58 -0400

    implementing LRU

---
+++ lab5/Lru.h
@@ -0,0 +1,20 @@
+#ifndef LRU_H
+#define LRU_H
+
+
+#include "Linklist.h"
+#include <vector>
+
+class Lru{
+	private:
+		std::vector<Linklist> Map;
+		Linklist Procs;
+	public:
+		void initMap(int value); //Ensure Map is large enough to map index to process number 
+		void hitPage(int page, int proc); //Move page and process to the front of the Lru
+		int getLruProc(); //return the LRU process
+		int getLruPage(int proc); // return the LRU page for a process
+		int hitNewPage(int page, int proc); //Put new page from an existing proc into the linklist
+};
+
+#endif
